f74345acda2b9cc89d2c257fb7e29aa2e342f1d5
KingOcyfo <osifoemynos@gmail.com>
2022-07-03 12:22:47 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
