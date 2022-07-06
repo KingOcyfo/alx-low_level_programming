01d0ba9db35df19e3cd6fac47cbf81c216b0c8c5
KingOcyfo <osifoemynos@gmail.com>
2022-07-06 06:56:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Decsription: prints alphabet lowercase\n
+ * Return: Always 0
+ */
+int main(void)
+{
+char j = 'a';
+while (j <= 'z')
+{
+_putchar (j);
+j++;
+}
+_putchar('\n');
+return (0);
+}
