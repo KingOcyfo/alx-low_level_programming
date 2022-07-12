8737f22af36a7e18e2f5f3e8338530923466f933
KingOcyfo <osifoemynos@gmail.com>
2022-07-12 13:53:49 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,8 +2,8 @@
 
 /**
  * rev_string - prints a string in reverse
- * @s: string to be reversed
- * Return: 0
+ * @s: the used string to be reversed
+ * Return : string
  */
 
 void rev_string(char *s)
@@ -12,14 +12,14 @@ int len, i, half;
 char temp;
 
 for (len = 0; s[len] != '\0'; len++)
-{
+;
 i = 0;
 half = len / 2;
-}
+
 while (half--)
 {
 temp = s[len - i - 1];
-s[len - i - 1] = s[1];
+s[len - i - 1] = s[i];
 s[i] = temp;
 i++;
 }
